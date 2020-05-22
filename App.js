cbacd7272e504f1591edf4f4211efbad934b8bf8
adludo <alex@alexs-air-8.home>
2020-05-22 18:14:12 +1000

    basic list maker - it works now

--- App.js
+++ App.js
@@ -18,18 +18,56 @@ import {
 
 
 export default class App extends React.Component {
+  state = {
+    title: '',
+    author: '',
+    books: [],
+  };
+
+  onChangeText = (key, val) => {
+    this.setState({ [key]: val });
+  };
+
   render() {
     return (
-      <View>
-        <Text>Hello World!</Text>
+      <View stlye={styles.container}>
+        <TextInput
+          style={styles.input}
+          value={this.state.title}
+          onChangeText={val => this.onChangeText('title', val)}
+          placeholder='What do you want to read?'
+        />
+
+        <TextInput
+          style={styles.input}
+          value={this.state.author}
+          onChangeText={val => this.onChangeText('author', val)}
+          placeholder='Who wrote it?'
+        />
+
+        <Button
+          onPress={() => alert('Success')}
+          title='Add to TBR'
+          color='#eeaa55'
+        />
       </View >
     );
   }
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    padding: 50,
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 10,
+    paddingTop: 50,
+  },
+
+  input: {
+    height: 50,
+    borderBottomWidth: 2,
+    borderBottomColor: 'blue',
+    marginVertical: 10,
   },
 });
 
